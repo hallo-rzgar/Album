f857ebe88efada35f4ae521a29b71057f0ef40a9
halop <halopolo99@gmail.com>
2021-10-25 23:08:40 +0300

    index & create

--- photoShow/resources/views/albums/create.blade.php
+++ photoShow/resources/views/albums/create.blade.php
@@ -1,2 +1,16 @@
-Create
+@extends('layout.app')
+@section('content')
+    <h2 class="font-weight-bold mb-4">Create Albums</h2>
+
+
+    {!!Form::open(['action' => 'App\Http\Controllers\AlbumsController@store','method' => 'POST', 'enctype' => 'multipart/form-data'])!!}
+        {{ Form::bsText('name') }}
+        {{ Form::bsTextarea('Description') }}
+        {{Form::file('cover_image',['class'=>'form-control-file'])}}
+        {{ Form::bsSubmit('Submit','submit' ,['class'=>'btn btn-primary mt-4']) }}
+        {!! Form::close() !!}
+
+
+@endsection
+
 
