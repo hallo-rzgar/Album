45b3235ab4869c60b6dedde1a21830b9114690e2
halop <halopolo99@gmail.com>
2021-10-27 17:15:07 +0300

    create photos

--- photoShow/resources/views/photos/show.blade.php
+++ photoShow/resources/views/photos/show.blade.php
@@ -1,4 +1,16 @@
 @extends('layout.app')
 @section('content')
-    <h3>$photo->title</h3>
+    <h3>{{$photo->title}}</h3>
+    <p>{{$photo->description}}</p>
+    <a class="btn btn-secondary btn-sm" href="/albums/{{$photo->album_id}}">Back To Gallery</a>
+    <hr>
+    <img  style="width:500px; height: auto;margin-top:15px" src="/storage/photos/{{$photo->album_id}}\{{$photo->photo}}" alt="{{$photo->title}}">
+    <br><br>
+    {!!Form::open(['action' => ['App\Http\Controllers\PhotosController@destroy', $photo->id],'method' => 'POST'])!!}
+    {{ Form::hidden('_method', 'DELETE') }}
+    {{ Form::bsSubmit('delete photo','delete' ,['class'=>'btn btn-danger mt-1']) }}
+    {!! Form::close() !!}
+
+
+
 @endsection
